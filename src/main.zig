03c3c500603b501c6e47b56ba6eafb019ecbd976
Luke Harwood <lukeharwood11@gmail.com>
2025-03-08 11:54:39 -0600

    It compiles

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const proxz = @import("proxz");
 
-const ArrayList = std.ArrayListUnmanaged(InternalStep);
+const ArrayList = std.ArrayListUnmanaged;
 
 // const agentz = @import("agentz");
 //
@@ -200,12 +200,9 @@ pub const Tool = struct {
 pub const ToolManager = struct {
     // Note: refactor to use String type from zig-string or some equivalent (or make your own Luke:)
     // Maybe this should have a reference to the agent? And then the Tool has access to it's manager?
-    tools: []const Tool,
-
-    pub const ToolOutput = struct {
-        exit: bool = false,
-        content: []const u8,
-    };
+    // TODO: future luke: does this need to be a pointer?
+    tools: *std.ArrayList(Tool),
+    allocator: std.mem.Allocator,
 
     const return_tool: Tool = .{
         .name = "return",
@@ -229,10 +226,41 @@ pub const ToolManager = struct {
         .config = .{ .exit = true },
     };
 
+    pub fn init(allocator: std.mem.Allocator, tools: []const Tool) !ToolManager {
+        const arr = try allocator.create(std.ArrayList(Tool));
+        arr.* = std.ArrayList(Tool).init(allocator);
+        // add all user defined tools
+        try arr.appendSlice(tools);
+        // allows the agent to exit and respond
+        try arr.append(return_tool);
+        return .{
+            .tools = arr,
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *ToolManager) void {
+        self.tools.deinit();
+        self.allocator.destroy(self.tools);
+    }
+
+    pub fn register(self: *ToolManager, tool: Tool) !void {
+        try self.tools.append(tool);
+    }
+
+    pub fn registerMany(self: *ToolManager, tools: []Tool) !void {
+        try self.tools.appendSlice(tools);
+    }
+
+    pub const ToolOutput = struct {
+        exit: bool = false,
+        content: []const u8,
+    };
+
     // should pass in an arena allocator
     pub fn describe(self: *const ToolManager, allocator: std.mem.Allocator) ![]const u8 {
         var tool_text: []const u8 = undefined;
-        for (self.tools, 0..) |tool, i| {
+        for (self.tools.items, 0..) |tool, i| {
             const description = try tool.describe(allocator);
             if (i != 0) {
                 tool_text = try std.fmt.allocPrint(allocator, "{s}, {s}", .{ tool_text, description });
@@ -253,7 +281,7 @@ pub const ToolManager = struct {
             return .{ .content = "Failed to parse parameters." };
         };
         // TODO: error catch
-        for (self.tools) |tool| {
+        for (self.tools.items) |tool| {
             std.log.info("'{s}' <=> '{s}'", .{ tool.name, step.tool });
             if (std.mem.eql(u8, tool.name, step.tool)) {
                 const tool_output = tool.execute(allocator, parameters.object) catch {
@@ -269,116 +297,129 @@ pub const ToolManager = struct {
     }
 };
 
-pub fn getWeather(tool: *const Tool, allocator: std.mem.Allocator, params: std.json.ObjectMap) ![]const u8 {
-    _ = allocator;
-    _ = params;
-    _ = tool;
-    return "53 and sunny - low chance of rain";
-}
+pub const Agent = struct {
+    config: AgentConfig,
+    arena: *std.heap.ArenaAllocator,
+    tool_manager: ToolManager,
+    openai: *proxz.OpenAI,
 
-const weather_tool: Tool = .{
-    .name = "get_weather",
-    .description = "Get's weather for the given city",
-    .params = &[_]Tool.Parameter{.{
-        .name = "city",
-        .dtype = .string,
-        .description = "The city to search",
-    }},
-    .toolFn = getWeather,
+    pub const AgentConfig = struct {
+        tools: []const Tool,
+        system_prompt: []const u8 = "You are a helpful assistant.",
+    };
+
+    pub fn init(allocator: std.mem.Allocator, config: AgentConfig) !Agent {
+        const arena = try allocator.create(std.heap.ArenaAllocator);
+        arena.* = std.heap.ArenaAllocator.init(allocator);
+        const openai = try proxz.OpenAI.init(allocator, .{});
+        const manager = try ToolManager.init(allocator, config.tools);
+        return .{
+            .config = config,
+            .arena = arena,
+            .tool_manager = manager,
+            .openai = openai,
+        };
+    }
+
+    pub fn deinit(self: *Agent) void {
+        self.arena.deinit();
+        self.openai.deinit();
+
+        self.arena.child_allocator.destroy(self.arena);
+    }
+
+    pub fn execute(self: *Agent, input: []const u8) ![]const u8 {
+        const allocator = self.arena.child_allocator;
+        var internal_steps = try ArrayList(InternalStep).initCapacity(allocator, 2);
+        defer internal_steps.deinit(allocator);
+        var cnt: usize = 0;
+        // TODO: add config for max number of steps
+        for (0..4) |_| {
+            var step_string: []const u8 = "";
+            for (internal_steps.items, 0..) |step, i| {
+                step_string = try std.fmt.allocPrint(self.arena.allocator(), "{s}{s}{s}", .{
+                    step_string,
+                    if (i == 0) "" else "\n",
+                    try step.formatPrompt(self.arena.allocator()),
+                });
+            }
+
+            const prompt = try std.fmt.allocPrint(allocator, XML_REACT_PROMPT, .{
+                self.config.system_prompt,
+                try self.tool_manager.describe(self.arena.allocator()),
+                input,
+                step_string,
+            });
+            defer allocator.free(prompt);
+
+            std.log.info("Prompt:\n{s}", .{prompt});
+
+            const response = try self.openai.chat.completions.create(.{
+                .model = "gpt-4o-mini",
+                .messages = &[_]ChatMessage{
+                    .{
+                        .role = "user",
+                        .content = prompt,
+                    },
+                },
+                .stop = &[_][]const u8{
+                    "observation: ",
+                },
+            });
+            defer response.deinit();
+
+            std.log.info("{s}\n==================================", .{response.choices[0].message.content});
+
+            var step = try InternalStep.parse(
+                self.arena.allocator(),
+                response.choices[0].message.content,
+            );
+
+            const output = try self.tool_manager.execute(
+                self.arena.allocator(),
+                step,
+            );
+            step.observe(output.content);
+            try internal_steps.append(allocator, step);
+            if (output.exit) {
+                return output.content;
+            }
+            cnt = cnt + 1;
+        }
+        return "Error ran out of steps...";
+    }
 };
-
-// const return_tool: Tool = .{
-//     .name = "return",
-//     .description = "when you're ready to respond to the user",
-//     .params = &[_]Tool.Parameter{
-//         .{
-//             .name = "text",
-//             .dtype = .string,
-//             .description = "response text",
-//         },
-//     },
-// };
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    var arena = std.heap.ArenaAllocator.init(allocator);
-    defer arena.deinit();
-
-    var openai = try proxz.OpenAI.init(allocator, .{});
-    defer openai.deinit();
-
-    var arr = try ArrayList.initCapacity(allocator, 2);
-    defer arr.deinit(allocator);
-
-    const manager: ToolManager = .{
-        .tools = &[_]Tool{
-            weather_tool,
-            ToolManager.return_tool,
-        },
+    const weather_tool: Tool = .{
+        .name = "get_weather",
+        .description = "Get's weather for the given city",
+        .params = &[_]Tool.Parameter{.{
+            .name = "city",
+            .dtype = .string,
+            .description = "The city to search",
+        }},
+        .toolFn = struct {
+            pub fn func(_: *const Tool, _: std.mem.Allocator, _: std.json.ObjectMap) ![]const u8 {
+                return "53 and sunny - low chance of rain";
+            }
+        }.func,
     };
 
-    var done = false;
-    var internal_steps = try ArrayList.initCapacity(allocator, 2);
-    defer internal_steps.deinit(allocator);
+    const tools = &[_]Tool{
+        weather_tool,
+    };
 
-    var cnt: usize = 0;
+    var agent = try Agent.init(allocator, .{
+        .system_prompt = "Answer in old english!",
+        .tools = tools,
+    });
+    defer agent.deinit();
 
-    while (!done) {
-        var step_string: []const u8 = "";
-
-        for (internal_steps.items, 0..) |step, i| {
-            step_string = try std.fmt.allocPrint(arena.allocator(), "{s}{s}{s}", .{
-                step_string,
-                if (i == 0) "" else "\n",
-                try step.formatPrompt(arena.allocator()),
-            });
-        }
-
-        const prompt = try std.fmt.allocPrint(allocator, XML_REACT_PROMPT, .{
-            "You are a helpful agent who must use tools to answer the user's prompt/question.",
-            try manager.describe(arena.allocator()),
-            "What is the weather in New Berlin?",
-            step_string,
-        });
-        defer allocator.free(prompt);
-
-        std.log.info("Prompt:\n{s}", .{prompt});
-
-        const response = try openai.chat.completions.create(.{
-            .model = "gpt-4o-mini",
-            .messages = &[_]ChatMessage{
-                .{
-                    .role = "user",
-                    .content = prompt,
-                },
-            },
-            .stop = &[_][]const u8{
-                "observation: ",
-            },
-        });
-        defer response.deinit();
-
-        std.log.info("{s}\n==================================", .{response.choices[0].message.content});
-
-        var step = try InternalStep.parse(
-            arena.allocator(),
-            response.choices[0].message.content,
-        );
-
-        const output = try manager.execute(
-            arena.allocator(),
-            step,
-        );
-        std.log.info("Output: {s}", .{output.content});
-        step.observe(output.content);
-        try internal_steps.append(allocator, step);
-        if (output.exit) {
-            done = true;
-            std.log.info("=================== OUTPUT =================\n'{s}'", .{output.content});
-        }
-        cnt = cnt + 1;
-    }
+    const response = try agent.execute("What is the weather in new berlin?");
+    std.log.info("Main Output: {s}", .{response});
 }
