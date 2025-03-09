46e29cfbb3a3854b468b4778415171a5a67ed9a3
Luke Harwood <lukeharwood11@gmail.com>
2025-03-08 18:30:02 -0600

    Intermittent commit

--- agentz/llm/base.zig
+++ agentz/llm/base.zig
@@ -1,1 +1,20 @@
 const std = @import("std");
+
+pub const ChatMessage = struct {
+    role: []const u8,
+    content: []const u8,
+};
+
+pub const LLM = struct {
+    ptr: *anyopaque,
+    chatFn: *const fn (ptr: *anyopaque, messages: []const ChatMessage) anyerror!void,
+    chatStreamFn: *const fn (ptr: *anyopaque, messages: []const ChatMessage) anyerror!void,
+
+    pub fn chat(self: *const LLM, messages: []const ChatMessage) ![]const u8 {
+        return self.chatFn(self.ptr, messages);
+    }
+
+    pub fn chatStream(self: *const LLM, messages: []const ChatMessage) ![]const u8 {
+        return self.chatStream(self.ptr, messages);
+    }
+};
--- agentz/llm/openai.zig
+++ agentz/llm/openai.zig
@@ -1,1 +1,48 @@
+const std = @import("std");
 const proxz = @import("proxz");
+const base = @import("base.zig");
+
+const ArrayList = std.ArrayListUnmanaged;
+
+pub const ChatOpenAI = struct {
+    openai: *proxz.OpenAI,
+
+    pub fn init(allocator: std.mem.Allocator, config: proxz.OpenAIConfig) !ChatOpenAI {
+        const openai = try proxz.OpenAI.init(allocator, config);
+        return .{
+            .openai = openai,
+        };
+    }
+
+    pub fn deinit(self: *const ChatOpenAI) void {
+        self.openai.deinit();
+    }
+
+    pub fn chat(ptr: *anyopaque, messages: []const base.ChatMessage) ![]const u8 {
+        const self: *ChatOpenAI = @ptrCast(@alignCast(ptr));
+        const allocator = self.openai.allocator;
+        var request_messages = try ArrayList(proxz.ChatMessage).initCapacity(allocator, messages.len);
+        for (messages) |message| {
+            try request_messages.append(
+                allocator,
+                .{
+                    .role = message.role,
+                    .content = message.content,
+                },
+            );
+        }
+
+        // TODO: actually implement this.
+        const response = try self.openai.chat.completions.create();
+        defer response.deinit();
+    }
+    pub fn chatStream(_: *anyopaque, _: base.ChatMessage) ![]const u8 {}
+
+    pub fn llm(self: *ChatOpenAI) base.LLM {
+        return .{
+            .ptr = self,
+            .chatFn = chat,
+            .chatStreamFn = chatStream,
+        };
+    }
+};
